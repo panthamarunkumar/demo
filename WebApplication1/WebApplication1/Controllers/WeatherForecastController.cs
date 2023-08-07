bd279a1f9fab9e7d1062f5635b15b8db28ef39c8
panthamarunkumar <70441456+panthamarunkumar@users.noreply.github.com>
2023-08-07 15:46:42 +0530

    ini

--- WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Co00o0ol", "Mild", "Warm", "Balmy", "Hoirigriririrt", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Co00o0ol", "Mild", "Warm", "Balmy", "Hoirigrirjhjjnbjnirirt", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
